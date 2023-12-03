3f0c3de39c7cada2b033f894f7b38f1ba86da5f6
jurek-m <jurek.michniewicz@accenture.com>
2023-12-03 21:11:05 +0100

    3rd day - second star gained

--- scripts/aoc_3.py
+++ scripts/aoc_3.py
@@ -1,4 +1,5 @@
 import numpy as np
+from itertools import groupby
 
 
 def get_adjacent_subarr(arr, ix):
@@ -49,9 +50,60 @@ def identify_numbers(arr):
     return numbers, sum_part_nrs
 
 
-lists = list()
+def get_abs_ix(aster_index_subarr, ix):
+
+    a = ix[0]-1+aster_index_subarr[0] if ix[0]-1 >= 0 else aster_index_subarr[0]
+    c = ix[1]-1+aster_index_subarr[1] if ix[1]-1 >= 0 else aster_index_subarr[1]
+
+    return [a, c]
+
+
+def identify_gears(arr):
+    
+    gear_nrs = list()
+    temp_nr = {'number': 0, 'gear_attached': False, 'aster_pos': [], 'elems': {}}
+
+    for ix, elem in np.ndenumerate(arr):
+        # print(ix, elem)
+
+        if elem.isdigit():
+            
+            # print(ix, elem)
+            temp_nr['elems'][ix] = elem                                     # add elem to current number
+
+            # check if asterisk is adjacent
+            adjacent_fields = get_adjacent_subarr(arr, ix)
+            is_aster = (adjacent_fields == '*')
+            asterisk_adjacent = is_aster.any()
+
+            if asterisk_adjacent:
+                temp_nr['gear_attached'] = True
+                np.where(is_aster)[0]
+                for pos in range(len(np.where(is_aster)[0])):
+                    aster_index_subarr = [np.where(is_aster)[0][pos], np.where(is_aster)[1][pos]]
+                    aster_index_arr = get_abs_ix(aster_index_subarr, ix)
+                    # print(aster_index_arr, arr[aster_index_arr[0], aster_index_arr[1]])
+                    temp_nr['aster_pos'].append(aster_index_arr)
+            
+            # identify end of a number
+            if (ix[1] == len(arr)-1) or (not arr[ix[0], ix[1]+1].isdigit()):
+                
+                temp_nr['number'] = int("".join(temp_nr['elems'].values()))
+                
+                # close the number
+                gear_nrs.append(temp_nr)                                                        # add temp_nr to numbers
+                temp_nr = {'number': 0, 'gear_attached': False, 'aster_pos': [], 'elems': {}}   # clear temp_nr
+    
+    return gear_nrs
+
+
+def all_equal(iterable):
+    g = groupby(iterable)
+    return next(g, True) and not next(g, False)
+
 
 # change input to an array
+lists = list()
 with open("inputs/aoc_input_3.txt", "r") as lines:
     for line in lines:
         line = line.replace('\n', '')
@@ -59,6 +111,37 @@ with open("inputs/aoc_input_3.txt", "r") as lines:
         lists.append(lst)
 arr = np.array(lists)
 
+
+# 1 ------------------------------------------
+
 numbers, sum_part_nrs = identify_numbers(arr)
 
 print(sum_part_nrs)
+
+# 2 ------------------------------------------
+
+gear_nrs = identify_gears(arr)
+gears = dict()
+
+# check results and match gear numbers
+for nr in gear_nrs:
+    if nr['gear_attached'] is True:
+        if nr['aster_pos'] == []:
+            print('no positions')
+        if not all_equal(nr['aster_pos']):
+            print('not all positions are equal')
+        pos_key = tuple(nr['aster_pos'][0])             # change position list to tuple
+        if pos_key in gears.keys():
+            gears[pos_key].append(nr['number'])
+        else:
+            gears[pos_key] = [nr['number']]
+
+# calc sum of gear ratios
+gear_ratios_sum = 0
+for nrs in gears.values():
+    if len(nrs) > 2:
+        print('more than 2 numbers attached to asterisk')
+    elif len(nrs) == 2:
+        gear_ratios_sum += np.prod(nrs)
+
+print(gear_ratios_sum)
